5357c71c192367e0272672d1455e713c393953c4
jalal keramati <93380673+jkeramati@users.noreply.github.com>
2022-02-14 23:57:20 +0330

    set package of tests

--- discount/models.py
+++ discount/models.py
@@ -5,5 +5,5 @@ from core.models import *
 # Create your models here.
 class Discount(BaseModel):
     code = models.CharField(max_length=10, null=True, blank=True)
-    type_disc = models.BooleanField(null=True, blank=True)
-    value = models.FloatField()
+    type_disc = models.CharField(choices=[('PR', 'price'), ('VAL', 'value')])
+    value = models.IntegerField()
