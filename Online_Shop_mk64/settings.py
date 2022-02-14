875576ca2374628930a243d63166ad64afa2ff26
jalal keramati <93380673+jkeramati@users.noreply.github.com>
2022-02-15 01:02:22 +0330

    install app in setting

--- Online_Shop_mk64/settings.py
+++ Online_Shop_mk64/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'comment',
+    'core',
+    'discount',
+    'costumer',
+    'order',
+    'product',
 ]
 
 MIDDLEWARE = [
