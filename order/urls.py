e7ea90cf25b985ae3a5a27a01855d6ff0bfa4747
jalal keramati <93380673+jkeramati@users.noreply.github.com>
2022-02-15 03:35:34 +0330

    list view and url in order app

--- order/urls.py
+++ order/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from order.views import CartListView, CartItemListView
 
 urlpatterns = [
-    path('costumer/', CartListView.as_view(), 'cart_list_view'),
-    path('Address/', CartItemListView.as_view(), 'cartitem_list_view'),
+    path('cart/', CartListView.as_view(), 'cart_list_view'),
+    path('cartitem/', CartItemListView.as_view(), 'cartitem_list_view'),
 ]
