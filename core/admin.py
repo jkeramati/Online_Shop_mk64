78379a36772ca678435ed1f3c36a6d948b0ebc9a
jalal keramati <93380673+jkeramati@users.noreply.github.com>
2022-02-26 01:14:20 +0330

    fix admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,50 @@
 from django.contrib import admin
-
+from django.utils.translation import gettext_lazy as _
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
 
 from .models import User
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-UserAdmin.list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff')
-UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
-UserAdmin.ordering = ('phone',)
-UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
+from .form import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('phone', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('phone', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active','date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    list_editable = ('is_active',)
+    search_fields = ('phone',)
+    ordering = ('phone',)
+
+
+admin.site.register(User, CustomUserAdmin)
+
+# @admin.register(User)
+# class UesrAdminModel(admin.ModelAdmin):
+#     list_display = ['__str__', 'is_superuser', 'is_active']
+#     list_editable = ['is_active']
+#     search_fields = ['phone']
+#     ordering = ['phone']
+#     fieldsets = (
+#         (None, {'fields': ('phone', 'password')}),
+#         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+#         (_('Permissions'), {
+#             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+#         }),
+#         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+#     )
